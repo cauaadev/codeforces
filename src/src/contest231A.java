19eaf4afc4ba3a36f3d2be9bf4a1f1c2a4cc2128
Caua Diniz <cauasouzadev@gmail.com>
2026-05-27 17:23:01 -0300

    solucao 231A code forces

--- src/src/contest231A.java
+++ src/src/contest231A.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class contest231A {
-}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int colunas = sc.nextInt();
+        var linhas = 3;
+git
+        int[][] matriz = new int[colunas][linhas];
+        if(colunas >= 1 && colunas <= 1000){
+            for (int i = 0; i < colunas; i++){
+                for (int j = 0; j < linhas; j++){
+                    matriz[i][j] = sc.nextInt();
+                }
+            }
+        }
+        int problemSolved = 0;
+        for (int i = 0; i < colunas; i++){
+            Set<Integer> vistos = new HashSet<>();
+            for (int j = 0; j < linhas; j++){
+                if(!vistos.add(matriz[i][j]) && matriz[i][j] == 1){
+                    problemSolved++;
+                    break;
+                }
+            }
+        }
+        System.out.println(problemSolved);
+
+    }
+        }
+
