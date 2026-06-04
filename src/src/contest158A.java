73d70a67b6e60d184a519fee87a5c43a3eb4e3db
Caua Diniz <cauasouzadev@gmail.com>
2026-06-03 21:53:33 -0300

    Update contest158A and contest231A

--- src/src/contest158A.java
+++ src/src/contest158A.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class contest158a {
+public class contest158A {
+    public static void main(String[] args) {
+        int numPart = 0;
+        int k = 0;
+        int counterWinners = 0;
+        Scanner sc = new Scanner(System.in);
+        numPart = sc.nextInt();
+        k = sc.nextInt();
+        if (k >= 1 && k <= numPart && numPart <= 50) {
+            int[] lista = new int[numPart];
+            for (int i = 0; i < lista.length; i++) {
+                int valor = sc.nextInt();
+                if (valor > 0 && valor <= 100) {
+                    lista[i] = valor;
+                }
+
+            }
+            for (int i = 0; i < lista.length; i++) {
+                if (lista[i] >= lista[k - 1] && lista[i] > 0) {
+                    counterWinners++;
+                }
+            }
+            System.out.println(counterWinners);
+        }
+    }
 }
